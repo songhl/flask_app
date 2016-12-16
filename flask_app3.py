a7872e4bf7b9e607ec473097f78110ad05b1cbc1
songhl <songhl519@163.com>
2016-12-16 12:05:57 +0800

    测试成功

--- flask_app3.py
+++ flask_app3.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 #coding:utf-8
 #time: 2016/12/12 17:59
-# 消息提示和异常处理
-from flask import Flask,flash,render_template
+from flask import Flask,flash,render_template,request
 app = Flask(__name__)
 app.secret_key = '123'
 @app.route("/")
 def hello_world():
     flash("hello wutong")
     return render_template("index3.html")
+@app.route("/login",methods=["POST","GET"])
+def login():
+    form =request.form
+    username=form.get("username")
+    password=form.get("password")
+    if not username:
+        flash("please input username")
+        return render_template("index3.html")
+    if not password:
+        flash("please input password")
+        return render_template("index3.html")
+    if username == "songhailong" and password == "hailong":
+        flash("login success")
+        return render_template("index3.html")
+    else:
+        flash("username or password is wrong")
+        return render_template("index3.html")
 if __name__ == '__main__':
     app.run()
